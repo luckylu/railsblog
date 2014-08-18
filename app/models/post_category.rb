d90e66940aa2d1efaead9bf029b1a47ed18a944a
Michael Lu <646179989@qq.com>
2014-08-18 22:38:54 +0800

    working on frontend 10%

--- app/models/post_category.rb
+++
@@ -1,2 +0,0 @@
-class PostCategory < ActiveRecord::Base
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+15.times do |a|
+Post.create(title:"Hello World #{a}",content:"Goodbye #{a}",user_id:"1")
+end
+1.upto(6) do |x|
+	CategoryPost.create(post_id:"#{x}", category_id:"1")
+end
+1.upto(6) do |x|
+	CategoryPost.create(post_id:"#{x}", category_id:"2")
+end
+7.upto(10) do |x|
+	CategoryPost.create(post_id:"#{x}", category_id:"2")
+end
+11.upto(15) do |x|
+	CategoryPost.create(post_id:"#{x}", category_id:"3")
+end
+11.upto(15) do |x|
+	CategoryPost.create(post_id:"#{x}", category_id:"4")
+end
+
+4.times do |x|
+	Category.create(name:"category#{x}")
+end
+User.create(username:"aa",email:"aa@hp.com",password:"aaaaaaaa",admin:"true")
